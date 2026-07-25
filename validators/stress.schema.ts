0c83cd998531ec26746126116f4438d291442d3f
sovsianykov <serhii.ovsianykov@dataart.com>
2026-07-25 19:33:57 +0200

    init 6

--- validators/stress.schema.ts
+++ validators/stress.schema.ts
@@ -5,10 +5,12 @@ export const StressSchema = z.object({
     .string()
     .regex(/^\d{2}:\d{2}$/, 'Неверный формат времени (HH:mm)'),
   level: z
-    .number({ invalid_type_error: 'Укажите уровень стресса' })
-    .int()
-    .min(1, 'Минимальный уровень: 1')
-    .max(10, 'Максимальный уровень: 10'),
+      .number({
+        message: 'Укажите уровень стресса',
+      })
+      .int()
+      .min(1, 'Минимальный уровень: 1')
+      .max(10, 'Максимальный уровень: 10'),
   reason: z.string().max(500).optional().or(z.literal('')),
 })
 
